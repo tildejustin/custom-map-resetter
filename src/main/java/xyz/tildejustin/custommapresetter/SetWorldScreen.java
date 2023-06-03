b7b6e4c0c0b20374263a4e1a1b8056ce0e589024
tildejustin <spacepepper3.14@gmail.com>
2023-06-03 17:20:07 -0400

    bump version

--- src/main/java/xyz/tildejustin/custommapresetter/SetWorldScreen.java
+++ src/main/java/xyz/tildejustin/custommapresetter/SetWorldScreen.java
@@ -24,22 +24,20 @@ public class SetWorldScreen extends SelectWorldScreen {
         ((SelectWorldScreenAccessor)this).setRecreateButton(new ButtonWidget(0, 0, 0, 0, "", (buttonWidget) -> {}));
 
 
+        assert this.minecraft != null;
         this.minecraft.keyboard.enableRepeatEvents(true);
         ((SelectWorldScreenAccessor)this).setSearchBox(new TextFieldWidget(this.font, this.width / 2 - 100, 22, 200, 20, this.searchBox, I18n.translate("selectWorld.search")));
-        ((SelectWorldScreenAccessor)this).getSearchBox().setChangedListener((string) -> {
-            ((SelectWorldScreenAccessor)this).getLevelList().filter(() -> string, false);
-        });
+        ((SelectWorldScreenAccessor)this).getSearchBox().setChangedListener((string) -> ((SelectWorldScreenAccessor)this).getLevelList().filter(() -> string, false));
         ((SelectWorldScreenAccessor)this).setLevelList(new WorldListWidget(this, this.minecraft, this.width, this.height, 48, this.height - 64, 36, () -> ((SelectWorldScreenAccessor)this).getSearchBox().getText(), ((SelectWorldScreenAccessor)this).getLevelList()));
         this.children.add(((SelectWorldScreenAccessor)this).getSearchBox());
         this.children.add(((SelectWorldScreenAccessor)this).getLevelList());
         ButtonWidget selectButton = this.addButton(new ButtonWidget(this.width / 2 - (150 / 2) - 150 - 5, this.height - 28, 150, 20, I18n.translate("selectWorld.select"), (buttonWidget) -> {
+            //noinspection OptionalGetWithoutIsPresent
             CustomMapResetter.resetTracker.setCurrentWorld(((SelectWorldScreenAccessor)this).getLevelList().method_20159().get().level.getName());
             this.minecraft.openScreen(parent);
         }));
         ((SelectWorldScreenAccessor)this).setSelectButton(selectButton);
-        this.addButton(new ButtonWidget(this.width / 2 + (150 / 2) + 5, this.height - 28, 150, 20, I18n.translate("gui.cancel"), (buttonWidget) -> {
-            this.minecraft.openScreen(this.parent);
-        }));
+        this.addButton(new ButtonWidget(this.width / 2 + (150 / 2) + 5, this.height - 28, 150, 20, I18n.translate("gui.cancel"), (buttonWidget) -> this.minecraft.openScreen(this.parent)));
         this.addButton(new ButtonWidget(this.width / 2 - 75, this.height - 28, 150, 20, "Delete Session Worlds", (buttonWidget) -> {
             CustomMapResetter.resetTracker.deleteWorlds();
             ((SelectWorldScreenAccessor)this).getLevelList().filter(() -> "", false);
