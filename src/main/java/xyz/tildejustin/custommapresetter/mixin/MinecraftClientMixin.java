447c8ee980605b6e84981957b18d91cde8636c9d
tildejustin <spacepepper3.14@gmail.com>
2025-07-22 01:05:35 -0400

    skip experimental settings warning, @Dynamic

--- src/main/java/xyz/tildejustin/custommapresetter/mixin/MinecraftClientMixin.java
+++ src/main/java/xyz/tildejustin/custommapresetter/mixin/MinecraftClientMixin.java
@@ -1,9 +1,8 @@
 package xyz.tildejustin.custommapresetter.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.tildejustin.custommapresetter.CustomMapResetter;
 
@@ -11,6 +10,7 @@ import java.util.concurrent.CompletableFuture;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
+    @Dynamic
     @Inject(method = {"reloadResources(Z)Ljava/util/concurrent/CompletableFuture;", "method_36561(ZLnet/minecraft/class_310$class_8764;)Ljava/util/concurrent/CompletableFuture;"}, at = @At(value = "HEAD"), cancellable = true)
     private void custommapresetter$stopResourceReload(CallbackInfoReturnable<CompletableFuture<Void>> cir) {
         if (CustomMapResetter.running) {
